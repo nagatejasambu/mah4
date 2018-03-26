b91957ac62d487671ed4b9efe829859737839346
root <root@ip-172-31-26-205.us-west-2.compute.internal>
2018-03-26 14:26:14 +0000

    hi this is

---
+++ src/main/java/cjpcloud/in/com/App.java
@@ -0,0 +1,13 @@
+package cjpcloud.in.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
